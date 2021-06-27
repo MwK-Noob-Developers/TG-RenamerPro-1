f0a761cacd6c1c8cdbdf63c07e54eef2f0ed8222
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-06-27 17:18:13 +0530

    Update main_filter.py

--- mwk/rename/main_filter.py
+++ mwk/rename/main_filter.py
@@ -39,13 +39,13 @@ async def rename_filter(c,m):
       filename = None 
     
     text += "Select the desired Option"
-    button.append([InlineKeyboardButton("📂 Rename as File 📂", callback_data="rename_file")])
+    button.append([InlineKeyboardButton("Rename as File", callback_data="rename_file")])
   # Thanks to albert for mime_type suggestion 
     if media.mime_type.startswith("video/"):
     ## how the f the other formats can be uploaded as video 
-      button.append([InlineKeyboardButton("🎞️ Rename as Video 🎞️",callback_data="rename_video")])
-      button.append([InlineKeyboardButton("🎞️ Convert to File 📂",callback_data="convert_file")])
-      button.append([InlineKeyboardButton("📂 Convert to Video 🎞️",callback_data="convert_video")])
+      button.append([InlineKeyboardButton("Rename as Video",callback_data="rename_video")])
+      button.append([InlineKeyboardButton("Convert to File",callback_data="convert_file")])
+      button.append([InlineKeyboardButton("Convert to Video",callback_data="convert_video")])
     button.append([InlineKeyboardButton("Cancel ❌",callback_data="cancel")])
  
     markup = InlineKeyboardMarkup(button)
