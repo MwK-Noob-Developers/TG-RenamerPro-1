2bbc1f0fbec63d28cedb7e55e1ebfec11c1408e5
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-07-22 07:35:45 +0530

    Update broadcast.py

--- mwk/shamil/broadcast.py
+++ mwk/shamil/broadcast.py
@@ -5,8 +5,8 @@ import asyncio
 import datetime
 import aiofiles.os
 import traceback
-from configs import Config
-from helpers.database.access_db import db
+from mwk.config import Config
+from database.access_db import db
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
 
