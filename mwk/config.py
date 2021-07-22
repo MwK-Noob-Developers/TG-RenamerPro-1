db5795269797cb700507fc26c1d74ab76957f397
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-07-22 09:51:34 +0530

    Update config.py

--- mwk/config.py
+++ mwk/config.py
@@ -11,5 +11,5 @@ class Config(object):
   CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION",False)
   UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1271358814)
-  MONGODB_URI = os.environ.get("MONGODB_URI", "")
+  MONGODB_URI = os.environ.get("MONGODB_URI",)
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
